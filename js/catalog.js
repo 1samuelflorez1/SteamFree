a2989eb86d203491d645af6f8f4dbf9eca959541
Natt - <nordonez376@gmail.com>
2025-05-23 15:34:37 -0500

    se agrego la funcion de la  barra de busqueda

--- js/catalog.js
+++ js/catalog.js
@@ -284,3 +284,45 @@ contenedor.addEventListener("click", function(e) {
     window.location.href = "product.html"
   }
 })
+
+//Barra de busqueda
+const searchInput = document.getElementById("search-input")
+
+searchInput.addEventListener("input", () => {
+  const searchTerm = searchInput.value.toLowerCase()
+  contenedor.innerHTML = "" 
+
+  const juegosFiltrados = juegos.filter(juego =>
+    juego.title.toLowerCase().includes(searchTerm)
+  )
+  
+  if (juegosFiltrados.length === 0) {
+    contenedor.innerHTML = "<p>No se encontraron juegos.</p>"
+  } else {
+    juegosFiltrados.forEach((juego, index) => {
+      const gameContainer = document.createElement("div")
+      gameContainer.classList.add("game-container")
+
+      const titulo = juego.title || juego.nombre;
+      const descripcion = juego.descripcion || juego.short_description
+      const thumbnail = juego.thumbnail
+
+      gameContainer.innerHTML = `
+        <div class="cards">
+          <div class="img-card">
+            <img src="${thumbnail}" alt="${titulo}">
+          </div>
+          <div class="text-card">
+            <div class="name-icon">
+              <h3>${titulo}</h3><i class='bx bx-heart'></i>
+            </div>
+            <p>${descripcion}</p>
+          </div>
+        </div>
+        <button class="button-card" data-index="${juegos.indexOf(juego)}">Info</button>
+      `;
+
+      contenedor.appendChild(gameContainer)
+    });
+  }
+});
